7063dec2c29bbf21122c1ed99acaa1a7b9b6baf2
Adam Schwartz <adam@anschwa.com>
2019-10-30 21:12:04 -0400

    start using gopls

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ag-executable "ag")
+ '(message-send-mail-function (quote message-send-mail-with-sendmail))
+ '(package-selected-packages
+   (quote
+    (spinner go-mode company lsp-ui company-lsp lsp-mode git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag fzf avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode geiser slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish)))
+ '(send-mail-function (quote mailclient-send-it)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(lsp-ui-sideline-code-action ((t (:inherit highlight))))
+ '(web-mode-doctype-face ((t nil)))
+ '(web-mode-html-attr-name-face ((t nil)))
+ '(web-mode-html-tag-face ((t nil)))
+ '(whitespace-space ((t (:background "pink" :foreground "darkgrey")))))
