78dd64d027ef66e985ad038a5587dc38c9311da9
Adam Schwartz <aschwartz@diagnotes.com>
2019-11-06 09:09:33 -0500

    make magit-status fullscreen

--- custom.el
+++ custom.el
@@ -4,10 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ag-executable "ag")
+ '(magit-display-buffer-function (quote magit-display-buffer-fullframe-status-v1))
  '(message-send-mail-function (quote message-send-mail-with-sendmail))
  '(package-selected-packages
    (quote
-    (spinner go-mode company lsp-ui company-lsp lsp-mode git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag fzf avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode geiser slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish)))
+    (company-lsp lsp-ui lsp-mode gnu-elpa-keyring-update spinner go-mode company git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag fzf avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode geiser slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish)))
  '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
