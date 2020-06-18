025faee9ed3057ac188fec29b55ea6851d518cb1
Adam Schwartz <aschwartz@diagnotes.com>
2020-06-18 13:25:36 -0400

    add hooks for M-x compile

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
  '(message-send-mail-function (quote message-send-mail-with-sendmail))
  '(package-selected-packages
    (quote
-    (go-errcheck string-inflection go-add-tags epresent company-lsp lsp-ui lsp-mode gnu-elpa-keyring-update spinner go-mode company git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag fzf avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode geiser slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish)))
+    (wgrep-ag nginx-mode htmlize dockerfile-mode go-errcheck string-inflection go-add-tags epresent company-lsp lsp-ui lsp-mode gnu-elpa-keyring-update spinner go-mode company git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag fzf avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode geiser slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish)))
  '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
