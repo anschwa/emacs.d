28c97c09e9739f7255c77c1d965380c16f83ff43
Adam Schwartz <adad@anschwa.com>
2020-09-09 22:18:37 -0400

    updates for macOS

--- custom.el
+++ custom.el
@@ -4,12 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ag-executable "ag")
- '(magit-display-buffer-function (quote magit-display-buffer-fullframe-status-v1))
- '(message-send-mail-function (quote message-send-mail-with-sendmail))
+ '(magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+ '(message-send-mail-function 'message-send-mail-with-sendmail)
  '(package-selected-packages
-   (quote
-    (elixir-mode wgrep-ag nginx-mode htmlize dockerfile-mode go-errcheck string-inflection go-add-tags epresent company-lsp lsp-ui lsp-mode gnu-elpa-keyring-update spinner go-mode company git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag fzf avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode geiser slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish)))
- '(send-mail-function (quote mailclient-send-it)))
+   '(exec-path-from-shell elixir-mode wgrep-ag nginx-mode htmlize dockerfile-mode go-errcheck string-inflection go-add-tags epresent company-lsp lsp-ui lsp-mode gnu-elpa-keyring-update spinner go-mode company git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag fzf avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode geiser slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish))
+ '(send-mail-function 'mailclient-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
