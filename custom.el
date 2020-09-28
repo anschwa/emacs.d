6f719fec31ab3c74fcea3d1a9d0009d8aabd21ea
Adam Schwartz <adam@anschwa.com>
2020-09-27 21:42:33 -0400

    add docker and elixir packages

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  '(magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
  '(message-send-mail-function 'message-send-mail-with-sendmail)
  '(package-selected-packages
-   '(exec-path-from-shell elixir-mode wgrep-ag nginx-mode htmlize dockerfile-mode go-errcheck string-inflection go-add-tags epresent company-lsp lsp-ui lsp-mode gnu-elpa-keyring-update spinner go-mode company git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag fzf avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode geiser slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish))
+   '(fzf docker-compose-mode exec-path-from-shell elixir-mode wgrep-ag nginx-mode htmlize dockerfile-mode go-errcheck string-inflection go-add-tags epresent company-lsp lsp-ui lsp-mode gnu-elpa-keyring-update spinner go-mode company git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode geiser slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish))
  '(send-mail-function 'mailclient-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -18,4 +18,5 @@
  '(web-mode-doctype-face ((t nil)))
  '(web-mode-html-attr-name-face ((t nil)))
  '(web-mode-html-tag-face ((t nil)))
+ '(web-mode-symbol-face ((t (:weight bold))))
  '(whitespace-space ((t (:background "pink" :foreground "darkgrey")))))
