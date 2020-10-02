8ba79daffb0ab5c0bfa1576ca2b1e96c8c2625bc
Adam Schwartz <adam@anschwa.com>
2020-10-02 16:23:29 -0400

    set org-agenda-files

--- custom.el
+++ custom.el
@@ -4,11 +4,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ag-executable "ag")
- '(magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
- '(message-send-mail-function 'message-send-mail-with-sendmail)
+ '(custom-safe-themes
+   (quote
+    ("2654583d8f0f16654148eb969119b6dee6223dc25e668b3b5d39f0b26c814649" default)))
+ '(magit-display-buffer-function (quote magit-display-buffer-fullframe-status-v1))
+ '(message-send-mail-function (quote message-send-mail-with-sendmail))
  '(package-selected-packages
+
    '(fzf docker-compose-mode exec-path-from-shell elixir-mode wgrep-ag nginx-mode htmlize dockerfile-mode go-errcheck string-inflection go-add-tags epresent company-lsp lsp-ui lsp-mode gnu-elpa-keyring-update spinner go-mode company git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode geiser slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish))
  '(send-mail-function 'mailclient-send-it))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
