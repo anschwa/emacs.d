36e17ec9bb417016d20fd7ca93e3a8bfe5ea1024
Adam Schwartz <adam@anschwa.com>
2021-08-25 13:10:41 -0400

    add shellcheck

--- custom.el
+++ custom.el
@@ -5,14 +5,12 @@
  ;; If there is more than one, they won't work right.
  '(ag-executable "ag")
  '(custom-safe-themes
-   (quote
-    ("2654583d8f0f16654148eb969119b6dee6223dc25e668b3b5d39f0b26c814649" default)))
- '(magit-display-buffer-function (quote magit-display-buffer-fullframe-status-v1))
- '(message-send-mail-function (quote message-send-mail-with-sendmail))
+   '("2654583d8f0f16654148eb969119b6dee6223dc25e668b3b5d39f0b26c814649" default))
+ '(magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+ '(message-send-mail-function 'message-send-mail-with-sendmail)
  '(package-selected-packages
-   (quote
-    (vterm go-guru rfc-mode rust-mode ledger-mode fzf docker-compose-mode exec-path-from-shell elixir-mode wgrep-ag nginx-mode htmlize dockerfile-mode go-errcheck string-inflection go-add-tags epresent company-lsp lsp-ui lsp-mode gnu-elpa-keyring-update spinner go-mode company git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish)))
- '(send-mail-function (quote mailclient-send-it)))
+   '(flymake-shellcheck vterm go-guru rfc-mode rust-mode ledger-mode fzf docker-compose-mode exec-path-from-shell elixir-mode wgrep-ag nginx-mode htmlize dockerfile-mode go-errcheck string-inflection go-add-tags epresent company-lsp lsp-ui lsp-mode gnu-elpa-keyring-update spinner go-mode company git-link jedi blacken docker-tramp expand-region multiple-cursors flycheck rainbow-mode dumb-jump ag avy smex pdf-tools writeroom-mode ibuffer-vc fish-mode magit markdown-mode yaml-mode slime protobuf-mode haskell-mode json-mode angular-mode coffee-mode js2-mode haml-mode emmet-mode web-mode company-jedi use-package diminish))
+ '(send-mail-function 'mailclient-send-it))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
