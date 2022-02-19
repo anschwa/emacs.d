74e4ea8032fb868d0160a28b05b87ee754a0b37b
Adam Schwartz <adam@anschwa.com>
2022-02-18 18:33:13 -0600

    make sure org-mode can export styled html code blocks

--- custom.el
+++ custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   '(htmlize web-mode use-package smex rg rainbow-mode modus-operandi-theme magit go-mode git-link fzf flymake-shellcheck expand-region emmet-mode eglot)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
