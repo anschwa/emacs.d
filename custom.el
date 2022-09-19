bb4639b65b803af71fe52b6c24f349154c364399
Adam Schwartz <adam@anschwa.com>
2022-09-19 18:16:11 -0400

    emacs 28.1

--- custom.el
+++ custom.el
@@ -3,17 +3,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("a0415d8fc6aeec455376f0cbcc1bee5f8c408295d1c2b9a1336db6947b89dd98" "dad40020beea412623b04507a4c185079bff4dcea20a93d8f8451acb6afc8358" default))
  '(package-selected-packages
    '(htmlize web-mode use-package smex rg rainbow-mode modus-operandi-theme magit go-mode git-link fzf flymake-shellcheck expand-region emmet-mode eglot)))
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(lsp-ui-sideline-code-action ((t (:inherit highlight))))
- '(web-mode-doctype-face ((t nil)))
- '(web-mode-html-attr-name-face ((t nil)))
- '(web-mode-html-tag-face ((t nil)))
- '(web-mode-symbol-face ((t (:weight bold))))
- '(whitespace-space ((t (:background "pink" :foreground "darkgrey")))))
+ '(font-lock-builtin-face ((t (:inherit modus-themes-bold :foreground "nil" :weight bold))))
+ '(font-lock-constant-face ((t (:foreground "nil"))))
+ '(font-lock-function-name-face ((t (:foreground "nil"))))
+ '(font-lock-keyword-face ((t (:foreground "nil"))))
+ '(font-lock-negation-char-face ((t (:foreground "nil"))))
+ '(font-lock-type-face ((t (:foreground "nil"))))
+ '(font-lock-variable-name-face ((t (:foreground "nil"))))
+ '(web-mode-doctype-face ((t (:inherit font-lock-builtin-face))))
+ '(web-mode-html-attr-name-face ((t :foreground "nil")))
+ '(web-mode-html-attr-value-face ((t (:inherit font-lock-string-face))))
+ '(web-mode-html-tag-face ((t :foreground "nil")))
+ '(web-mode-symbol-face ((t (:weight bold)))))
