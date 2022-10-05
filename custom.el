a8f723a9db0d9e1cc444e9fbbc8d8be50890c0fe
Adam Schwartz <adam@anschwa.com>
2022-10-05 15:27:53 -0400

    update ruby-mode

--- custom.el
+++ custom.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("a0415d8fc6aeec455376f0cbcc1bee5f8c408295d1c2b9a1336db6947b89dd98" "dad40020beea412623b04507a4c185079bff4dcea20a93d8f8451acb6afc8358" default))
+   '(default))
  '(package-selected-packages
-   '(htmlize web-mode use-package smex rg rainbow-mode modus-operandi-theme magit go-mode git-link fzf flymake-shellcheck expand-region emmet-mode eglot)))
+   '()))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -15,7 +15,7 @@
  '(font-lock-builtin-face ((t (:inherit modus-themes-bold :foreground "nil" :weight bold))))
  '(font-lock-constant-face ((t (:foreground "nil"))))
  '(font-lock-function-name-face ((t (:foreground "nil"))))
- '(font-lock-keyword-face ((t (:foreground "nil"))))
+ '(font-lock-keyword-face ((t (:inherit modus-themes-bold :foreground "nil" :weight bold))))
  '(font-lock-negation-char-face ((t (:foreground "nil"))))
  '(font-lock-type-face ((t (:foreground "nil"))))
  '(font-lock-variable-name-face ((t (:foreground "nil"))))
