2ae0fa38bd4664f27fd46e9f5bb4ac649cc97c45
Adam Schwartz <schwa@kant.local>
2022-10-23 20:33:05 -0400

    replace ido+smex with icomplete+fido

--- custom.el
+++ custom.el
@@ -11,15 +11,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-builtin-face ((t (:inherit modus-themes-bold :foreground "nil" :weight bold))))
- '(font-lock-constant-face ((t (:foreground "nil"))))
- '(font-lock-function-name-face ((t (:foreground "nil"))))
- '(font-lock-keyword-face ((t (:inherit modus-themes-bold :foreground "nil" :weight bold))))
- '(font-lock-negation-char-face ((t (:foreground "nil"))))
- '(font-lock-type-face ((t (:foreground "nil"))))
- '(font-lock-variable-name-face ((t (:foreground "nil"))))
- '(web-mode-doctype-face ((t (:inherit font-lock-builtin-face))))
- '(web-mode-html-attr-name-face ((t :foreground "nil")))
- '(web-mode-html-attr-value-face ((t (:inherit font-lock-string-face))))
- '(web-mode-html-tag-face ((t :foreground "nil")))
- '(web-mode-symbol-face ((t (:weight bold)))))
+)
