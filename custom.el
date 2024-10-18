d9985b131e8238caf37cddbfa3bafceea5098fc1
Adam Schwartz <adam@anschwa.com>
2024-10-18 17:59:59 -0400

    bring back custom defuns

--- custom.el
+++ custom.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bfc0b9c3de0382e452a878a1fb4726e1302bf9da20e69d6ec1cd1d5d82f61e3d" default))
+   '("b29ba9bfdb34d71ecf3322951425a73d825fb2c002434282d2e0e8c44fce8185" "bfc0b9c3de0382e452a878a1fb4726e1302bf9da20e69d6ec1cd1d5d82f61e3d" default))
  '(package-selected-packages
-   '(add-node-modules-path prettier-js web-mode use-package smex rubocop rspec-mode rg rainbow-mode modus-operandi-theme magit htmlize go-mode git-link fzf flymake-shellcheck f expand-region emmet-mode eglot)))
+   '(inf-ruby sqlformat add-node-modules-path prettier-js web-mode use-package smex rubocop rspec-mode rg rainbow-mode modus-operandi-theme magit htmlize go-mode git-link fzf flymake-shellcheck f expand-region emmet-mode eglot)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
